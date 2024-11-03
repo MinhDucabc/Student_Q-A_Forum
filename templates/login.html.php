93dc2bcf87d7955d4f2df8de7d9141339dbb9c38
minhduc1222 <frostgamer150@gmail.com>
2024-11-03 14:40:58 +0700

    2. login

--- templates/login.html.php
+++ templates/login.html.php
@@ -8,5 +8,13 @@
 <body>
     <?php require_once "header.php"?>
     <button type="button" class="btn btn-primary">Primary</button>
+    <form action="login.php" method="post">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" class="form-control">
     <?php require_once "footer.php"?>
 </body>
